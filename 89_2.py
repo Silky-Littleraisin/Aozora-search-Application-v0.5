8d1cbc7317688e633a38da83d779979a1be8f13c
Silky-Littleraisin <rangersmiku@gmail.com>
2019-03-25 18:43:57 +0900

    03255

--- 89_2.py
+++ 89_2.py
@@ -9,5 +9,9 @@ fname_out='corpus_80.txt'
 with bz2.open(fname_input,'rt') as data_file, open(fname_out,'wt') as output_file:
     for line in data_file:
         tokens=[]
-        for chunk in line.split(''):
-            token
+        for chunk in line.split(' '):
+            token= chunk.strip().strip('.,!?;:()[]\'"')
+            if len(token)>0:
+                tokens.append(token)
+        
+        print(*tokens, sep=' ', end='\n',file=output_file)
