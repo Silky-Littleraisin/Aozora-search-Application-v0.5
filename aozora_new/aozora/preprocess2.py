8a297ef827fba687855bd018950a37aee3f419a0
Silky-Littleraisin <rangersmiku@gmail.com>
2019-05-21 23:13:11 +0900

    0522

---
+++ aozora_new/aozora/preprocess2.py
@@ -0,0 +1,9 @@
+import os
+import re
+
+path = '/Users/silky/Documents/GitHub/textnovel/txt'
+
+for r, d, f in os.walk(path)
+    for file in f:
+        file = path + '/' + file
+        with open(file) as novel:
